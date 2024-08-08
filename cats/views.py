dac761ef7b5043ee8e0c4872d45e9697253052dd
Евгений Собянин <ea.sobyanin2023@gmail.com>
2024-08-08 12:20:07 +0300

    Завершение темы посвященной пермишенам, тротлингу, пагинации, фильтрации, поиску, сортировке

--- cats/views.py
+++ cats/views.py
@@ -1,16 +1,48 @@
+from rest_framework import permissions
 from rest_framework import viewsets
+from rest_framework.throttling import AnonRateThrottle, ScopedRateThrottle
+from rest_framework.pagination import LimitOffsetPagination
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters
 
 from .models import Achievement, Cat, User
-
+from .permissions import ReadOnly, OwnerOrReadOnly
 from .serializers import AchievementSerializer, CatSerializer, UserSerializer
+from .throttling import WorkingHoursRateThrottle
+from .pagination import CatsPagination
 
 
 class CatViewSet(viewsets.ModelViewSet):
     queryset = Cat.objects.all()
     serializer_class = CatSerializer
+    permission_classes = (OwnerOrReadOnly,)
+    #throttle_classes = (AnonRateThrottle,)  # Подключили класс AnonRateThrottle
+
+    # Если кастомный тротлинг-класс вернёт True - запросы будут обработаны
+    # Если он вернёт False - все запросы будут отклонены
+    throttle_classes = (WorkingHoursRateThrottle, ScopedRateThrottle)
+    # А далее применится лимит low_request
+    throttle_scope = 'low_request'
+
+    # Даже если на уровне проекта установлен PageNumberPagination
+    # Для котиков будет работать LimitOffsetPagination
+    # pagination_class = LimitOffsetPagination
+    pagination_class = None
+
+    filter_backends = (DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter)
+    filterset_fields = ('color', 'birth_year')
+    search_fields = ('name', )
+    ordering_fields = ('name', 'birth_year')
+    ordering = ('birth_year',) # сортировка по умолчанию
+
 
     def perform_create(self, serializer):
-        serializer.save(owner=self.request.user) 
+        serializer.save(owner=self.request.user)
+
+    def get_permissions(self):
+        if self.action == 'retrieve':
+            return (ReadOnly(), )
+        return super().get_permissions()
 
 
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
